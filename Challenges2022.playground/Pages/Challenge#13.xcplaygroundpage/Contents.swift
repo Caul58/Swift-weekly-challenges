22985723272a471db94c22e046becfb186aa7313
Carlos R. Pérez <caul58@gmail.com>
2024-03-29 23:30:47 +0100

    Includes recursive factorial challenge

---
+++ Challenges2022.playground/Pages/Challenge#13.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+/*:
+
+&nbsp;
+
+[< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+# Challenge 13: Recursive factorial
+
+ Write a function which calculates and returns factorial of a given number in a recursive way.
+ 
+ */
+import Foundation
+
+func recursiveFactorial(number: Int) -> Int? {
+    (number <= 0) ? nil : (number <= 1) ? 1 : number * (recursiveFactorial(number: number - 1)!)
+}
+
+func unwrapRecursiveFactorial(number: Int) -> String {
+    guard let result = recursiveFactorial(number: number) else {
+        return "No tiene factorial"
+    }
+    return String(result)
+}
+
+
+print("""
+    Test 1, factorial de 0:
+    \(unwrapRecursiveFactorial(number: 0))
+    
+    """)
+
+print("""
+    Test 2, factorial de 7:
+    \(unwrapRecursiveFactorial(number: 7))
+    
+    """)
+
+print("""
+    Test 3, factorial de 10:
+    \(unwrapRecursiveFactorial(number: 10))
+    
+    """)
+
+print("""
+    Test 4, factorial de 1:
+    \(unwrapRecursiveFactorial(number: 1))
+    
+    """)
+
+print("""
+    Test 5, factorial de -1:
+    \(unwrapRecursiveFactorial(number: -1))
+    
+    """)
+
+print("""
+    Test 6, factorial de 4:
+    \(unwrapRecursiveFactorial(number: 4))
+    
+    """)
+
+//: [Next](@next)
--- Challenges2022.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Challenges2022.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -21,6 +21,7 @@
  * [Challenge #10: Balanced Expressions](Challenge#10)
  * [Challenge #11: Disjoint Sets](Challenge#11)
  * [Challenge #12: Is it a palindrome?](Challenge#12)
+ * [Challenge #13: Recursive factorial](Challenge#13)
  * [...]
  * [Challenge #43: Special by Halloween](Challenge#43)
  * [Challenge #44: Boomerang](Challenge#44)
